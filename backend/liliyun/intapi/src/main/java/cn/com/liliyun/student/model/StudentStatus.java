5e660a737759a79ff94291e4920adbecb5f33db6
mozhengxin <mozhengxin@qq.com>
2018-03-19 13:26:13 +0800

    学员状态对象

---
+++ backend/liliyun/intapi/src/main/java/cn/com/liliyun/student/model/StudentStatus.java
@@ -0,0 +1,75 @@
+package cn.com.liliyun.student.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import cn.com.liliyun.common.model.BaseModel;
+
+public class StudentStatus extends BaseModel implements Serializable {
+
+	private Integer id;
+
+	private String name;
+
+	private int sort;
+
+	private Date ctime;
+
+	private Date mtime;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSort() {
+		return sort;
+	}
+
+	public void setSort(int sort) {
+		this.sort = sort;
+	}
+
+	public Date getCtime() {
+		return ctime;
+	}
+
+	public void setCtime(Date ctime) {
+		this.ctime = ctime;
+	}
+
+	public Date getMtime() {
+		return mtime;
+	}
+
+	public void setMtime(Date mtime) {
+		this.mtime = mtime;
+	}
+}
