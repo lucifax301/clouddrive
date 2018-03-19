72f49adbc4aa0891bb21432812aabfb4e8089617
mozhengxin <mozhengxin@qq.com>
2018-03-19 13:26:54 +0800

    接入层

---
+++ backend/liliyun/httpaccess/src/main/java/cn/com/liliyun/httpaccess/controller/StudentStatusController.java
@@ -0,0 +1,41 @@
+package cn.com.liliyun.httpaccess.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.com.liliyun.common.model.ResultBean;
+import cn.com.liliyun.trainorg.service.StudentStatusService;
+
+
+@Controller
+@ResponseBody
+@RequestMapping(value="/studentstatus")
+public class StudentStatusController extends BaseController {
+
+	@Autowired
+	private StudentStatusService studentStatusService;
+	
+	@RequestMapping(value="/edit")
+	public ResultBean edit(StudentStatus studentStatus) {
+		ResultBean rb;
+		if (studentStatus.getId() == null) {
+			rb = studentStatusService.addStudentStatus(studentStatus);
+		} else {
+			rb = studentStatusService.updateStudentStatus(studentStatus);
+		}
+		return rb;
+	}
+	
+	@RequestMapping(value="/list")
+	public ResultBean list(StudentStatus studentStatus) {
+		return studentStatusService.getList(studentStatus);
+	}
+	
+	@RequestMapping(value="/delete")
+	public ResultBean delete(StudentStatus studentStatus) {
+		return studentStatusService.deleteStudentStatus(studentStatus);
+	}
+		
+}
