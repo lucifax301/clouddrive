fce4d9cd2cc6de49158f6d1a5f28dd15fc0b309d
mozhengxin <mozhengxin@qq.com>
2018-03-22 09:15:53 +0800

    数据库表

--- backend/database/学员状态表.sql
+++ backend/database/学员状态表.sql
@@ -1,7 +1,6 @@
 CREATE TABLE `t_student_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `status` int(11) NOT NULL DEFAULT '0' COMMENT '学员学车状态',
-  `status_name` varchar(50) NOT NULL COMMENT '状态名称',
+  `name` varchar(50) NOT NULL COMMENT '状态名称',
   `sort` int(11) DEFAULT NULL COMMENT '状态顺序',
   `ctime` datetime DEFAULT NULL,
   `mtime` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
